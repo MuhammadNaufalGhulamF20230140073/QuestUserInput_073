65c56f2025401575d649045ca4cbadfa9b6accb6
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-10-30 12:46:52 +0700

    Tambah composable FormDataDiri untuk input data diri

--- app/src/main/java/com/example/inputprogram/TeksEdit.kt
+++ app/src/main/java/com/example/inputprogram/TeksEdit.kt
@@ -1,1 +1,30 @@
 package com.example.inputprogram
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+) {
