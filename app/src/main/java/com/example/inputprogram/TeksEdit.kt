9829792fe3fb0992e88fedcf1317ad11953fb650
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-01 10:56:49 +0700

    buat form pendaftaran dengan input dummy

--- app/src/main/java/com/example/inputprogram/TeksEdit.kt
+++ app/src/main/java/com/example/inputprogram/TeksEdit.kt
@@ -1,130 +1,45 @@
 package com.example.inputprogram
 
 
-import android.R.attr.text
+package com.example.inputprogram
+
+
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.inputprogram.ui.theme.LightBlue
+import com.example.inputprogram.ui.theme.LightPurple
+import com.example.inputprogram.ui.theme.Purple80
+import com.example.inputprogram.ui.theme.PurpleGrey40
+import com.example.inputprogram.ui.theme.PurpleGrey80
 
 @Composable
-fun FormDataDiri(modifier: Modifier
-) {
-    var textNama by remember { mutableStateOf("") }
-    var textAlamat by remember { mutableStateOf("") }
-    var textJK by remember { mutableStateOf("") }
-
-    var nama by remember { mutableStateOf("") }
-    var alamat by remember { mutableStateOf("") }
-    var jenis by remember { mutableStateOf("") }
-
-    val gender: List<String> = listOf("laki-laki", "perempuan")
-
-    Column(
-        modifier = Modifier.padding(top = 50.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(
-            value = textNama,
-            singleLine = true,
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier.width(250.dp),
-            label = { Text(text = "Nama Lengkap") },
-            onValueChange = {
-                textNama = it
-            }
-        )
-        Row {
-            gender.forEach { item ->
-                Row(
-                    modifier = Modifier.selectable(
-                        selected = textJK == item,
-                        onClick = { textJK = item }
-                    ), verticalAlignment = Alignment.CenterVertically) {
-                    RadioButton(
-                        selected = textJK == item,
-                        onClick = {
-                            textJK = item
-                        })
-                    Text(item)
-                }
-            }
-        }
-        OutlinedTextField(
-            value = textAlamat,
-            singleLine = true,
-            modifier = Modifier.width(250.dp),
-            label = { Text(text = "Alamat Lengkap") },
-            onValueChange = {
-                textAlamat = it
-            }
-        )
-        Divider(
-            modifier.padding(
-                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
-                    R.dimen.padding_medium
-                )
-            ),
-            thickness = dimensionResource(R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
-        Button(
-            modifier = Modifier.fillMaxWidth(1f),
-            enabled = textAlamat.isNotEmpty(),
-            onClick = {
-                nama = textNama
-                jenis = textJK
-                alamat = textAlamat
-            }
-        ) {
-            Text(stringResource(R.string.submit))
-        }
-        Divider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
-                    id = R.dimen.padding_medium
-                )
-            ),
-            thickness = dimensionResource(R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
-        ElevatedCard(
-            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.Black),
-            modifier = Modifier
-                .height(100.dp)
-                .width(300.dp)
-        ) {
-            Column(modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),) {
-                Text(text = "Nama   :" + nama, color = Color.White)
-                Text(text = "Gender :" + jenis, color = Color.White)
-                Text(text = "Alamat :" + alamat, color = Color.White)
-            }
-        }
-    }
-}
+fun FormPendaftaran(modifier: Modifier) {
+    val dummyTextValue = remember { mutableStateOf("") }
+    val dummyOnValueChange: (String) -> Unit = {}
